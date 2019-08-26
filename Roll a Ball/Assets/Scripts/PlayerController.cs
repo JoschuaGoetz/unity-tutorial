d53856abbadf9a2ca5db72366a2161b14ab35276
Joschua <joschua.goetz@gmail.com>
2019-08-26 15:51:35 +0200

    Add too much stuff

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody player;
+    public float speed;
+
+    private void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    private void FixedUpdate()
+    {
+        float dirVertical = Input.GetAxis("Vertical") * speed;
+        float dirHorizontal = Input.GetAxis("Horizontal") * speed;
+
+        Vector3 movement = new Vector3(dirHorizontal, 0.0f, dirVertical);
+
+        player.AddForce(movement, ForceMode.Force);
+    }
+
+}
